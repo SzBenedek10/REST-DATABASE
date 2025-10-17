8e2b20a84b9cf2e4d2f661ef31b71bf343822550
SzBenedek10 <xd.gamerbhd@gmail.com>
2025-10-17 11:27:19 +0200

    Add CRUD endpoints for user management

--- server.js
+++ server.js
@@ -42,10 +42,54 @@ app.post('/api/users',(req,res) =>{
         }
     })
 })
+//Get végpontok 
+app.get('/api/users', (req,res)=>{
+    db.all(`SELECT * FROM users`,[],(err, records) =>{
+        if(err) {
+            console.error(err)
+            res.status(500).json({message: 'Hiba történt'});
+        }else {
+            return res.status(200).json(records);
+        }
+    })
+})
+//Delete
+app.delete('/api/users/:id',(req, res) => {
+    const { id }= req.params;//id elérése
 
+    //Az adatbázis egy sorának a törlése 
+    db.run(`DELETE FROM users WHERE id = ?`, [id],
+    //Callback fügvény az esetleges törlési hiba kezelésésre
+    function (err) {
+        if (err){
+            return res.status(500).send(err.message);
+        }
+        else {
+             res.status(200).json({message: "Sikeres adattörlés"})
 
+        }
+    })
+})
 
+//PUT a táblázat adatainak a módosítása
+app.put('/api/users/:id', (req,res)=>{
+    const {id} = req.params;
+    const{firstName, lastName, city, address, phone, email, gender} = req.body;
 
+    const sql = 'UPDATE users SET firstName = ?, lastName = ?, city = ?, address = ?, phone = ?, email = ?, gender = ? WHERE id = ?';
+    db.run(sql, [firstName, lastName, city, address, phone, email, gender,id],
+        //Callback fügvény az esetleges hi ba keresésére
+         function (err) {
+        if (err){
+            return res.status(500).send(err.message);
+        }
+        else {
+             res.status(200).json({message: "A felhasználó frissítés megtörtént",id,firstName, lastName, city, address, phone, email, gender})
+
+        }
+    }
+    )
+} )
     app.listen(port, () => {
         console.log(`A szerver fut a htttp://localhost: ${port} címen`);
     })
