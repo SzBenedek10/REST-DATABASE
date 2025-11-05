8d6e31c2b0ba38c8d5110ec22942ab7c02c7cdb5
Mutyii0425 <csalimate35@gmail.com>
2025-11-05 10:27:00 +0100

    lasfds

---
+++ client.js
@@ -0,0 +1,34 @@
+//REST frontend - HTML kliens, az API elérésére és az adatbázis műveletekre
+
+const apiUrl = 'http://localhost:3000/api/users';
+const usersData = document.getElementById('usersData');
+
+//Az API elérése és az adatok lekérése
+
+async function getUsers() {
+    try  {
+        const response = await fetch(apiUrl); //Kapcsolódás az API-hoz
+        const users = await response.json();
+        usersData.innerHTML = users.map(user  => `
+            <tr>
+                <td>${user.id}</td>
+                <td>${user.firstName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.city}</td>
+                <td>${user.address}</td>
+                <td>${user.phone}</td>
+                <td>${user.email}</td>
+                <td>${user.gender}</td>
+                <td>
+                    <button>Törlés</button>
+                </td>
+            </tr>
+            `).join('');
+    }
+    catch (e) {
+        console.error(e.message);
+        alert('Hiba történt az adatok elérése során')
+
+    }
+}
+getUsers();// Az adatok lekérése szolgáló
